a55118158d057e384f6163b8d51ab2cbc61a0642
Iker Nafarrate <i.nafarrate@busman.es>
2023-03-28 19:50:03 +0200

    Fix org and user difference

--- github/create-git-branches.py
+++ github/create-git-branches.py
@@ -6,26 +6,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--token", help="Token de GitHub API")
 parser.add_argument("-m", "--module", help="Nombre del módulo a crear")
 parser.add_argument("-o", "--owner", help="Organización del repositorio")
-parser.add_argument("-u", "--user", help="Usuario del repositorio")
 parser.add_argument("-v", "--version", help="Versión")
 args = parser.parse_args()
 
 token = args.token
 module = args.module
 owner = args.owner
-user = args.user
 version = args.version
 
-if not token or not module or (not owner and not user) or not version:
+if not token or not module or not owner or not version:
     parser.error('No se han establecido los parámetros requeridos.')
 
-if owner and user:
-    parser.error('No se puede indicar una organización y un usuario al mismo tiempo.')
-
-is_org = True if owner else False
-user = user or owner
-
-branch_sha = rename_main_branch(token, module, user, version)
+branch_sha = rename_main_branch(token, module, owner, version)
 
 dev_branch = f"{version}_dev"
-create_new_branch(token, module, user, dev_branch, branch_sha)
+create_new_branch(token, module, owner, dev_branch, branch_sha)
--- github/create-git-repo.py
+++ github/create-git-repo.py
@@ -6,21 +6,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--token", help="Token de GitHub API")
 parser.add_argument("-m", "--module", help="Nombre del módulo a crear")
 parser.add_argument("-o", "--owner", help="Organización del repositorio")
-parser.add_argument("-u", "--user", help="Usuario del repositorio")
+parser.add_argument("--org", help="Es una compañia", action="store_true")
 args = parser.parse_args()
 
 token = args.token
 module = args.module
 owner = args.owner
-user = args.user
+is_org = args.org
 
-if not token or not module or (not owner and not user):
+if not token or not module or not owner:
     parser.error('No se han establecido los parámetros requeridos.')
 
-if owner and user:
-    parser.error('No se puede indicar una organización y un usuario al mismo tiempo.')
-
-is_org = True if owner else False
-user = user or owner
-
-create_repo(token, module, user, is_org)
+create_repo(token, module, owner, is_org)
