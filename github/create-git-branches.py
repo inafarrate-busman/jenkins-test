7ce7a367ddeac6614693515682cb19f7a9e10d2e
Iker Nafarrate <i.nafarrate@busman.es>
2023-03-28 19:18:39 +0200

    Complete github pipeline

--- github/create-git-branches.py
+++ github/create-git-branches.py
@@ -1,20 +1,22 @@
 import argparse
 
-from utils import create_repo, rename_main_branch, create_new_branch
+from utils import rename_main_branch, create_new_branch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--token", help="Token de GitHub API")
 parser.add_argument("-m", "--module", help="Nombre del módulo a crear")
 parser.add_argument("-o", "--owner", help="Organización del repositorio")
 parser.add_argument("-u", "--user", help="Usuario del repositorio")
+parser.add_argument("-v", "--version", help="Versión")
 args = parser.parse_args()
 
 token = args.token
 module = args.module
 owner = args.owner
 user = args.user
+version = args.version
 
-if not token or not module or (not owner and not user):
+if not token or not module or (not owner and not user) or not version:
     parser.error('No se han establecido los parámetros requeridos.')
 
 if owner and user:
@@ -23,4 +25,7 @@ if owner and user:
 is_org = True if owner else False
 user = user or owner
 
-create_repo(token, module, user, is_org)
+branch_sha = rename_main_branch(token, module, user, version)
+
+dev_branch = f"{version}_dev"
+create_new_branch(token, module, user, dev_branch, branch_sha)
--- github/utils.py
+++ github/utils.py
@@ -24,5 +24,61 @@ def create_repo(token, module, owner, is_org = True):
     response = requests.post(url, headers=headers, data=json.dumps(body))
 
     status_code = response.status_code
-    if status_code != 200:
+    if status_code != 201:
+        raise Exception(f"Status code: {status_code}. Url: {url}. Context: {response.text}")
+
+def rename_main_branch(token, module, owner, new_branch, main_branch = 'main'):
+    url = "%(base_url)s/repos/%(owner)s/%(module)s/branches/%(main_branch)s/rename" % {
+        'base_url': GITHUB_BASE_URL,
+        'owner': owner,
+        'module': module,
+        'main_branch': main_branch
+    }
+
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer %s' % token,
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    body = {
+        'new_name': new_branch,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+
+    status_code = response.status_code
+
+    if status_code != 201:
+        raise Exception(f"Status code: {status_code}. Url: {url}. Context: {response.text}")
+
+    result_data = response.json()
+
+    commit = result_data.get('commit', {})
+    branch_sha = commit.get('sha', False)
+
+    return branch_sha
+
+def create_new_branch(token, module, owner, new_branch, from_branch_sha):
+    url = "%(base_url)s/repos/%(owner)s/%(module)s/git/refs" % {
+        'base_url': GITHUB_BASE_URL,
+        'owner': owner,
+        'module': module
+    }
+
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer %s' % token,
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    body = {
+		"ref": "refs/heads/%(branch)s" % {"branch": new_branch},
+        "sha": from_branch_sha
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+    status_code = response.status_code
+
+    if status_code != 201:
         raise Exception(f"Status code: {status_code}. Url: {url}. Context: {response.text}")
